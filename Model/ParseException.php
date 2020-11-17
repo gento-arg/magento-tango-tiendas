32c962710dac1b6f5e67ae09e4fea9541a59489d
Manuel Canepa <manuelcanepa@gmail.com>
2020-11-17 16:36:17 -0300

    Add trace messages

---
+++ Model/ParseException.php
@@ -0,0 +1,11 @@
+<?php
+declare (strict_types = 1);
+
+namespace Gento\TangoTiendas\Model;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ParseException extends LocalizedException
+{
+
+}
